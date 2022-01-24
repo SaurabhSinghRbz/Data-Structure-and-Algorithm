0f04af7d72c3539dabc0d10b4f165548b37958d8
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-01-24 23:17:07 +0530

    Uploading Solutions

---
+++ Unit-2/Problems-A/6_ACM ICPC India Rank/ACM ICPC India Rank.js
@@ -0,0 +1,8 @@
+function ACM_ICPC_India_Rank(N, countriesArr) {
+    for (var i = 0; i < N; i++) {
+        if (countriesArr[i] == "India") {
+            console.log(i + 1)
+        }
+    }
+}
+ACM_ICPC_India_Rank()
