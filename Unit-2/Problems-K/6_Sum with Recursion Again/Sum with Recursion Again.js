aab3163abcaeeb0ab889f9dafa4c637fdc09f912
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-01-29 23:48:09 +0530

    Uloading Soltuion

---
+++ Unit-2/Problems-K/6_Sum with Recursion Again/Sum with Recursion Again.js
@@ -0,0 +1,40 @@
+function runProgram(input) {
+    var input = input.split('\n')
+    var t = +input[0]
+    var line = 1
+    for (var i = 1; i <= t; i++) {
+        var n = +input[line]
+        line++
+        var arr = input[line].split(" ").map(Number)
+        line++
+        console.log(rec(n, arr, 0))
+    }
+}
+function rec(n, arr, i) {
+    if (i == n - 1) {
+        return 0;
+    } else {
+        return Math.abs(arr[i] - arr[i + 1]) + rec(n, arr, i + 1)
+    }
+}
+
+if (process.env.USER === "") {
+    runProgram(``);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
