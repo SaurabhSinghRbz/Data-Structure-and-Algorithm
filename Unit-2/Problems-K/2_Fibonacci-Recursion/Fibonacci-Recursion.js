7e5eccf43eca23abcbd4f7b3645a7710e04df9de
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-01-29 23:40:40 +0530

    Uloading Soltuion

---
+++ Unit-2/Problems-K/2_Fibonacci-Recursion/Fibonacci-Recursion.js
@@ -0,0 +1,32 @@
+function runProgram(input) {
+    var n = +input
+    console.log(rec(n))
+}
+function rec(n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    } else {
+        return rec(n - 1) + rec(n - 2)
+    }
+}
+
+if (process.env.USER === "") {
+    runProgram(``);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
