4fcc4fd8c2d4ffe85e5419814d41f63fc82eb9dc
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-01-29 23:52:53 +0530

    Uloading Soltuion

---
+++ Unit-2/Problems-K/7_Beyond Factorial!/Beyond Factorial!.js
@@ -0,0 +1,33 @@
+function runProgram(input) {
+    var n = +input
+    rec(n);
+    console.log(Math.log(rec(n)).toFixed(4))
+}
+function rec(n) {
+    if (n == 1) {
+        return 1;
+    } else {
+        return n * rec(n - 1)
+    }
+}
+
+if (process.env.USER === "") {
+    runProgram(``);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
