857a6eeeabfb942fb18bc514643c1a6d821f0317
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-02-03 23:46:23 +0530

    Uploading Solution

---
+++ Unit-2/Problems-N/2_War of Minions/War of Minions.js
@@ -0,0 +1,39 @@
+function runProgram(input) {
+    var input = input.split("\n");
+    var n = +input[0];
+    var str = input[1].split("");
+    var stack = [];
+    for (var i = 0; i < n; i++) {
+        if (str[i] == stack[stack.length - 1]) {
+            stack.pop();
+        } else {
+            stack.push(str[i])
+        }
+    }
+    if (stack.length == 0) {
+        console.log(-1)
+    } else {
+        console.log(stack.join(""));
+    }
+
+}
+if (process.env.USER === "") {
+    runProgram(``);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
